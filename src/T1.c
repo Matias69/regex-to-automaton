a4ff0d0573654f7c739b43b49b4a34ad27c93f55
COMPARE MONCHO <matias.escobar.c@gmail.com>
2018-10-13 19:13:37 -0300

    crear función para chequear entrada

--- src/T1.c
+++ src/T1.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#MAX_ARGS_LEN 2
+#define MAX_ARGS_LEN 3
 
-boolean check_input(int, int);
+int check_input(int, int);
 
 int main(int argc, char *argv[]){
 	if(check_input(argc, MAX_ARGS_LEN)){
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
-boolean check_input(int input_len, int max_input_len){
-	if(input_len > max_input_len){ return false; }
-	return true;
+int check_input(int input_len, int max_input_len){
+	if(input_len > max_input_len){ return 0; }
+	return 1;
 }
