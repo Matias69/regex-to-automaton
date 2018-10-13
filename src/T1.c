4f3a0820f2c43e29fef862a41e21ae6ed76ed276
COMPARE MONCHO <matias.escobar.c@gmail.com>
2018-10-13 19:03:25 -0300

    nueva branch y algunos cambios

--- src/T1.c
+++ src/T1.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(){
-	printf("hello, world!\n");
+#MAX_ARGS_LEN 2
+
+boolean check_input(int, int);
+
+int main(int argc, char *argv[]){
+	if(check_input(argc, MAX_ARGS_LEN)){
+		printf("True!\n");
+	}else{
+		printf("False!!\n");
+	}
+
 	return 0;
+}
+
+boolean check_input(int input_len, int max_input_len){
+	if(input_len > max_input_len){ return false; }
+	return true;
 }
