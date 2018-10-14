f25b79b6d6a36e22a0b7c2d370217286ee57794f
COMPARE MONCHO <matias.escobar.c@gmail.com>
2018-10-14 16:47:12 -0300

    algunos cambios incompletos

--- src/T1.c
+++ src/T1.c
@@ -3,19 +3,36 @@
 
 #define MAX_ARGS_LEN 3
 
-int check_input(int, int);
+void check_input(int, char*[]);
+int check_input_len(int, int);
+int check_input_elements(char*[]);
+int belongs_to_sigma(char);
 
 int main(int argc, char *argv[]){
-	if(check_input(argc, MAX_ARGS_LEN)){
-		printf("True!\n");
-	}else{
-		printf("False!!\n");
-	}
+	//check_input(argc, argv);
+
+	printf("salida: %c\n", belongs_to_sigma(argv[2][0]));
 
 	return 0;
 }
 
-int check_input(int input_len, int max_input_len){
+void check_input(int argc, char *argv[]){
+	if(!check_input_len(argc, MAX_ARGS_LEN)){
+		printf("ERROR: cantidad de argumentos no valida, saliendo...\n");
+		exit(0);
+	}
+}
+int check_input_len(int input_len, int max_input_len){
 	if(input_len > max_input_len){ return 0; }
 	return 1;
+}
+
+int check_input_elements(char *argv[]){
+
+	return 1;
+}
+
+int belongs_to_sigma(char c){
+	if((c>=48 && c<=57)||(c>=65 && c<=90)||(c>=97 && c<=122)) return 1;
+	return 0;
 }
