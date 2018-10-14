4df66399fb7dfa27f0e1f7905c25080a06f855d5
COMPARE MONCHO <matias.escobar.c@gmail.com>
2018-10-14 17:55:16 -0300

    crear algunas funciones de chequeo para la entrada

--- src/T1.c
+++ src/T1.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define MAX_ARGS_LEN 3
 
 void check_input(int, char*[]);
 int check_input_len(int, int);
 int check_input_elements(char*[]);
+int check_regex_elements(char*[]);
+int belongs_to_regex_symbol(char);
 int belongs_to_sigma(char);
 
 int main(int argc, char *argv[]){
-	//check_input(argc, argv);
-
-	printf("salida: %c\n", belongs_to_sigma(argv[2][0]));
-
+	check_input(argc, argv);
+	printf("todo ok!\n");
 	return 0;
 }
 
@@ -21,15 +22,40 @@ void check_input(int argc, char *argv[]){
 		printf("ERROR: cantidad de argumentos no valida, saliendo...\n");
 		exit(0);
 	}
+	if(!check_input_elements(argv)){
+		printf("ERROR: los elementos de la entrada no pertenecen a sigma, saliendo...\n");
+		exit(0);
+	}
+	if(!check_regex_elements(argv)){
+		printf("ERROR: los elementos de la regex no son correctos, saliendo...\n");
+		exit(0);
+	}
 }
+
 int check_input_len(int input_len, int max_input_len){
 	if(input_len > max_input_len){ return 0; }
 	return 1;
 }
 
 int check_input_elements(char *argv[]){
-
+	char *input = argv[2];
+	int len = strlen(input);
+	for(int i=0; i<len; i++){	if(!belongs_to_sigma(input[i])) return 0;	}
 	return 1;
+}
+
+int check_regex_elements(char *argv[]){
+	char *regex = argv[1];
+	int len = strlen(regex);
+	for(int i=0; i<len; i++)
+		if(!(belongs_to_sigma(regex[i]) || belongs_to_regex_symbol(regex[i])))
+			return 0;
+	return 1;
+}
+
+int belongs_to_regex_symbol(char c){
+	if((c==33)||(c>=40 && c<=42)||(c==95)||(c==124)||(c==126)) return 1;
+	return 0;
 }
 
 int belongs_to_sigma(char c){
